be1caaf2656d508b0373032de3a65ce614789fb3
SusanMamu <susan.mamu99@gmail.com>
2024-01-18 16:50:12 +0300

    refine code

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT =100;
+        final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
 
